27892c478c84e7eeb7f03f50793a4d8a49973bbe
NekoBuster <mail@nekobuster.com>
2019-09-13 20:46:23 +1000

    init

---
+++ addresolution.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+monname=$(xrandr | grep connected | head -1 | cut -d " " -f 1)
+cvtout=$(cvt $1 $2)
+modeline=$(echo "$cvtout" | grep Modeline)
+modestr=$(echo "$modeline" | sed "s/Modeline\s//g")
+modename=$(echo "$modestr" | cut -d " " -f 1)
+xrandr --newmode $modestr
+xrandr --addmode $monname $modename
